9e818c0736c4462c6b13dd448bf03303ec467b27
sarath-jammoju <sarath.jammoju@sanjittechsolutions.in>
2025-01-31 12:26:28 +0530

    updated media queries

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,27 +13,65 @@ import epson_logo from '../assets/logos/epson_logo.webp'
 import canon_logo from '../assets/logos/canon_logo.webp'
 import brother_logo from '../assets/logos/brother_logo.webp';
 
+
+
+// Define logos array
+const partnerLogos = [
+  acer_logo,
+  asus_logo,
+  dell_logo,
+  hp_logo,
+  lenovo_logo,
+  samsung_logo,
+  zebronics_logo,
+  epson_logo,
+  canon_logo,
+  brother_logo,
+];
+
 const Carousel = () => {
+  // State to store the slidesToShow based on screen size
+  const [slidesToShow, setSlidesToShow] = useState(5);
+
+  // Update slidesToShow based on screen size
+  const updateSlidesToShow = () => {
+    if (window.innerWidth >= 374) {
+      setSlidesToShow(1); // Show 2 slides for smaller screens
+    }
+    if (window.innerWidth <= 600) {
+      setSlidesToShow(2); // Show 2 slides for smaller screens
+    } else if (window.innerWidth <= 768) {
+      setSlidesToShow(3); // Show 3 slides for tablets
+    } else if (window.innerWidth <= 1024) {
+      setSlidesToShow(4); // Show 4 slides for larger tablets
+    } else {
+      setSlidesToShow(5); // Show 5 slides for larger screens
+    }
+  };
+
+  // Use effect hook to update slidesToShow on window resize
+  useEffect(() => {
+    updateSlidesToShow(); // Set the initial slidesToShow based on screen size
+
+    // Add event listener for window resize to update slidesToShow
+    window.addEventListener('resize', updateSlidesToShow);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', updateSlidesToShow);
+    };
+  }, []);
+
   const settings = {
     dots: false,
     infinite: true,
-    slidesToShow: 5,
+    slidesToShow: slidesToShow, // Use the slidesToShow state here
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
     autoplaySpeed: 2000,
-    cssEase: "linear",
+    cssEase: 'linear',
   };
-
-   const partnerLogos = [
-      acer_logo,
-      asus_logo,
-      dell_logo,
-      hp_logo,
-      lenovo_logo,
-      samsung_logo,
-      zebronics_logo, epson_logo, canon_logo, brother_logo
-    ];
 
   return (
     <div className="slider-container">
